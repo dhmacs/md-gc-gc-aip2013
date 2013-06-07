ad787ce6bc2ad7b79ebe739f529afc66655ef277
student <student@studentvm.(none)>
2013-06-07 15:12:49 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MdGcGcAip2013::Application.config.secret_token = '5d2ecdcc20014e487ee844cf0ba51ba46e20c03f97151cc73a01e48244d307bfd1478de58717aad1c376907bef3c805b741a077c5777735d68ea27c12bc5f1d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MdGcGcAip2013::Application.config.session_store :cookie_store, key: '_md-gc-gc-aip2013_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MdGcGcAip2013::Application.config.session_store :active_record_store
